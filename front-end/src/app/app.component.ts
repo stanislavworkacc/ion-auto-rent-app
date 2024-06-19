8df8e6ba0bbd12e11c56c6213a93b1bb4f8f602c
demen <demenkdf@gmail.com>
2024-06-19 22:25:02 +0300

    trans

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,19 +1,38 @@
 import { Component } from '@angular/core';
 import {
-  IonApp,
+  IonApp, IonButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar,
 } from '@ionic/angular/standalone';
 import { logOutOutline } from 'ionicons/icons';
 import {addIcons} from "ionicons";
 import {RouterOutlet} from "@angular/router";
+import {TranslateModule, TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [IonApp, RouterOutlet],
+  imports: [IonApp, RouterOutlet, IonHeader, IonToolbar, IonTitle, IonContent, TranslateModule, IonButtons, IonButton],
 })
 export class AppComponent {
-  constructor() {
-    addIcons({ logOutOutline });
+
+  switchLanguage(language: string) {
+    this.translate.use(language);
+  }
+
+  constructor(private translate: TranslateService) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.translate.addLangs(['en', 'uk']);
+    this.translate.setDefaultLang('en');
+
+    const browserLang = this.translate.getBrowserLang();
+    if (browserLang) {
+      this.translate.use(browserLang.match(/en|uk/) ? browserLang : 'en');
+    } else {
+      this.translate.use('en');
+    }
   }
 }
+addIcons({ logOutOutline });
--- front-end/src/main.ts
+++ front-end/src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode } from '@angular/core';
+import {enableProdMode, importProvidersFrom} from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
@@ -6,9 +6,16 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
 
 if (environment.production) {
   enableProdMode();
+}
+
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
 bootstrapApplication(AppComponent, {
@@ -16,5 +23,15 @@ bootstrapApplication(AppComponent, {
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    importProvidersFrom(
+      HttpClientModule,
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+        }
+      })
+    )
   ],
 });
