5442bb42a4155599f240dc8efdf4e3fc53f55bfb
demen <demenkdf@gmail.com>
2024-06-23 12:50:38 +0300

    edit delete account

--- front-end/src/app/home/menu/menu-profile/profille-page/profile-edit-page/profile-edit-page.component.ts
+++ front-end/src/app/home/menu/menu-profile/profille-page/profile-edit-page/profile-edit-page.component.ts
@@ -1,20 +1,25 @@
 import {ChangeDetectionStrategy, Component, inject, OnInit} from '@angular/core';
 import {
+  AlertController,
+  IonAlert,
   IonAvatar,
   IonButton, IonButtons,
   IonContent,
-  IonHeader,
+  IonHeader, IonIcon,
   IonInput,
-  IonItem,
-  IonLabel, IonRange,
+  IonItem, IonItemDivider,
+  IonLabel, IonRange, IonSpinner,
   IonTitle,
   IonToolbar
 } from "@ionic/angular/standalone";
 import {BackButtonComponent} from "../../../../../shared/ui-kit/components/back-button/back-button.component";
 import {NotificationsPreviewComponent} from "../../notifications-preview/notifications-preview.component";
 import {NavController} from "@ionic/angular";
-import {FormsModule} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {NgClass, NgStyle} from "@angular/common";
+import {ValidateInputDirective} from "../../../../../shared/directives/validate-input.directive";
+import {matchingPasswordsValidator} from "../../../../../shared/utils/validators/matchingPasswordValidator";
+import {PhoneNumberFormatterDirective} from "../../../../../shared/directives/phone-formatter.directive";
 
 @Component({
   selector: 'app-profile-edit-page',
@@ -38,52 +43,143 @@ import {NgClass, NgStyle} from "@angular/common";
     FormsModule,
     NgStyle,
     NgClass,
+    IonIcon,
+    IonSpinner,
+    ValidateInputDirective,
+    ReactiveFormsModule,
+    PhoneNumberFormatterDirective,
+    IonItemDivider,
+    IonAlert,
   ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProfileEditPage implements OnInit {
 
   private navCtrl: NavController = inject(NavController);
+  private alertCtrl: AlertController = inject(AlertController);
+  private fb: FormBuilder = inject(FormBuilder);
 
-  profileCompletion: number = 70;
+  public form!: FormGroup;
+  public name!: FormControl;
+  public lastName!: FormControl;
+  public email!: FormControl;
+  public phone!: FormControl;
+  public oldPassword!: FormControl;
+  public password!: FormControl;
+  public confirmPassword!: FormControl;
+  public isFocused: { [key: string]: boolean } = {
+    name: false,
+    lastName: false,
+    google: false,
+    oldOpen: false,
+    lockOpen: false,
+    lockClosed: false,
+    phone: false,
+  };
 
-  getRangeGradient(value: number): string {
-    if (value <= 50) {
-      return `linear-gradient(to right,
-        #ff4d4d 0%,
-        #ff4d4d ${value}%,
-        #d1d5db ${value}%,
-        #d1d5db 100%)`;
-    } else if (value <= 90) {
-      return `linear-gradient(to right,
-        #ffcc00 0%,
-        #ffcc00 ${value}%,
-        #d1d5db ${value}%,
-        #d1d5db 100%)`;
-    } else {
-      return `linear-gradient(to right,
-        #4caf50 0%,
-        #4caf50 ${value}%,
-        #d1d5db ${value}%,
-        #d1d5db 100%)`;
-    }
-  }
+  public alertButtons = [
+    {
+      text: 'Відмінити',
+      role: 'cancel',
+      handler: () => {
 
-  getTextColor(value: number): string {
-    if (value <= 50) {
-      return 'text-red-600';
-    } else if (value <= 90) {
-      return 'text-yellow-600';
-    } else {
-      return 'text-green-600';
-    }
-  }
+      },
+    },
+    {
+      text: 'Підтвердити',
+      role: 'confirm',
+      handler: () => {
+        this.onDeleteAccount('confirm')
+      },
+    },
+  ];
+
+  showPassword: boolean = false;
+  showConfirmPassword: boolean = false;
+  isBlurred: boolean = true;
 
   goBack(): void {
     this.navCtrl.back()
   }
 
-  ngOnInit() {
+  onFocus(field: string): void {
+    this.isFocused[field] = true;
+  }
+
+  onBlur(field: string): void {
+    this.isFocused[field] = false;
+  }
+
+  toggleShowPassword(): void {
+    this.showPassword = !this.showPassword;
+  }
+
+  toggleShowConfirmPassword(): void {
+    this.showConfirmPassword = !this.showConfirmPassword;
+  }
+
+  onDeleteAccount(confirm: string): void {
+    if (confirm) {
+      this.presentAccountAlert()
+    }
+  }
+
+  async presentAccountAlert(): Promise<void> {
+    const alert: HTMLIonAlertElement = await this.alertCtrl.create({
+      header: 'Введіть пароль',
+      inputs: [
+        {
+          name: 'password',
+          type: 'password',
+          placeholder: 'Пароль'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Скасувати',
+          role: 'cancel',
+          handler: () => {
+          }
+        },
+        {
+          text: 'Підтвердити',
+          role: 'confirm',
+          handler: () => {
+          }
+        },
+      ]
+    });
+
+    await alert.present();
+  }
+
+  assignFormControls(): void {
+    this.name = this.form.get('name') as FormControl;
+    this.lastName = this.form.get('lastName') as FormControl;
+    this.email = this.form.get('email') as FormControl;
+    this.phone = this.form.get('phone') as FormControl;
+    this.oldPassword = this.form.get('oldPassword') as FormControl;
+    this.password = this.form.get('password') as FormControl;
+    this.confirmPassword = this.form.get('confirmPassword') as FormControl;
+  }
+
+  initForm(): void {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      lastName: ['', Validators.required],
+      phone: ['', [Validators.required, Validators.minLength(14)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      oldPassword: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', [Validators.required]]
+    }, {validator: matchingPasswordsValidator('password', 'confirmPassword')});
+
+    this.assignFormControls();
+  }
+
+
+  ngOnInit(): void {
+    this.initForm()
   }
 
 }
