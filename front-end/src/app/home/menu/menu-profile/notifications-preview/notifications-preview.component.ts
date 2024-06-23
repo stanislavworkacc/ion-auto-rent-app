d3d3b9e05c180d6f4d07d9a01c8c1d4ebc47a22a
demen <demenkdf@gmail.com>
2024-06-23 14:44:36 +0300

    add popovers

--- front-end/src/app/home/menu/menu-profile/notifications-preview/notifications-preview.component.ts
+++ front-end/src/app/home/menu/menu-profile/notifications-preview/notifications-preview.component.ts
@@ -1,5 +1,14 @@
-import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
-import {IonButton, IonIcon} from "@ionic/angular/standalone";
+import {ChangeDetectionStrategy, Component, inject, Input, OnInit} from '@angular/core';
+import {
+  IonButton, IonButtons,
+  IonContent,
+  IonIcon,
+  IonItem,
+  IonList,
+  IonPopover,
+  PopoverController
+} from "@ionic/angular/standalone";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'notifications-preview',
@@ -8,13 +17,31 @@ import {IonButton, IonIcon} from "@ionic/angular/standalone";
   standalone: true,
   imports: [
     IonButton,
-    IonIcon
+    IonIcon,
+    NgForOf,
+    IonContent,
+    IonItem,
+    IonList,
+    IonPopover,
+    IonButtons
   ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NotificationsPreviewComponent implements OnInit {
 
   @Input() styles: string;
+
+  socialMarks = [
+    {icon: 'notifications-outline', count: 3, description: 'Системні повідомлення', value: 'notifications'},
+    {icon: 'chatbubbles-outline', count: 3, description: 'Вхідні повідомлення', value: 'chat'},
+    {icon: 'heart-outline', count: 2, description: 'Вподобайки', value: 'likes'},
+    {icon: 'heart-dislike-outline', count: 2, description: 'Дизлайки', value: 'dislikes'}
+  ];
+
+  carsMarks = [
+    {icon: 'car-outline', count: 4, description: 'Транспортні засоби', value: 'car'},
+    {icon: 'checkmark-circle-outline', count: 4, description: 'Здано в оренду', value: 'checkmark'}
+  ];
 
   constructor() {
   }
