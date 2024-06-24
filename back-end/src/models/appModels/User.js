7d27b529121ff78d9c6b05a063ab6d3bdf8e1662
stasbelinsky <stanislav.belinskyi@homeemergencyassist.com>
2024-06-24 22:29:14 +0300

    Fix user registration

--- back-end/src/models/appModels/User.js
+++ back-end/src/models/appModels/User.js
@@ -2,9 +2,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const schema = new mongoose.Schema({
-    id: { type: mongoose.Schema.ObjectId, unique: true },
     email: { type: String, unique: true, required: true, trim: true},
-    // password: { type: String, required: true, trim: true},
     userName: { type: String, required: true, trim: true },
     enabled: { type: Boolean, default: true, required: true},
     // telephone: { type: String, unique: true, required: true, trim: true },
