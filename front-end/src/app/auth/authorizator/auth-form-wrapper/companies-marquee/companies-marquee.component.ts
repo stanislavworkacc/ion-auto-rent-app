0c520ce4c72744c81d2039d3f9a5a082fe5ecb87
demen <demenkdf@gmail.com>
2024-06-26 15:00:34 +0300

    add modal company desc

--- front-end/src/app/auth/authorizator/auth-form-wrapper/companies-marquee/companies-marquee.component.ts
+++ front-end/src/app/auth/authorizator/auth-form-wrapper/companies-marquee/companies-marquee.component.ts
@@ -1,7 +1,7 @@
 import {ChangeDetectionStrategy, Component, OnInit, signal, WritableSignal} from '@angular/core';
 import {NgForOf} from "@angular/common";
 import {IonicModule} from "@ionic/angular";
-import {IonIcon} from "@ionic/angular/standalone";
+import {IonButton, IonIcon, IonModal} from "@ionic/angular/standalone";
 
 @Component({
   selector: 'companies-marquee',
@@ -10,28 +10,44 @@ import {IonIcon} from "@ionic/angular/standalone";
   standalone: true,
   imports: [
     NgForOf,
-    IonIcon
+    IonIcon,
+    IonModal,
+    IonButton
   ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CompaniesMarqueeComponent  implements OnInit {
 
   public isPaused: WritableSignal<boolean> = signal(false);
-  public companies: WritableSignal<any> = signal([]);
+  public companies: WritableSignal<{ name: string, icon: string, description: string }[]> = signal([]);
+  public selectedCompany: WritableSignal<{ name: string, icon: string, description: string } | null> = signal(null);
+  public isModalOpen: WritableSignal<boolean> = signal(false);
 
   toggleAnimation(): void {
     this.isPaused.update((isPaused: boolean) => !isPaused);
   }
 
+  showCompanyDetails(company: { name: string, icon: string, description: string }): void {
+    this.toggleAnimation();
+    this.selectedCompany.set(company);
+    this.isModalOpen.set(true);
+  }
+
+  closeModal(): void {
+    this.isModalOpen.set(false);
+    this.selectedCompany.set(null);
+    this.toggleAnimation()
+  }
+
   setCompanies(): void {
     this.companies.set([
-      { name: 'Company A', icon: 'logo-apple' },
-      { name: 'Company B', icon: 'logo-google' },
-      { name: 'Company C', icon: 'logo-microsoft' },
-      { name: 'Company D', icon: 'logo-facebook' },
-      { name: 'Company E', icon: 'logo-twitter' },
-      { name: 'Company F', icon: 'logo-amazon' }
-    ])
+      { name: 'Company A', icon: 'logo-apple', description: 'Apple Inc. is an American multinational technology company.' },
+      { name: 'Company B', icon: 'logo-google', description: 'Google LLC is an American multinational technology company.' },
+      { name: 'Company C', icon: 'logo-microsoft', description: 'Microsoft Corporation is an American multinational technology company.' },
+      { name: 'Company D', icon: 'logo-facebook', description: 'Facebook is an American online social media and social networking service.' },
+      { name: 'Company E', icon: 'logo-twitter', description: 'Twitter is an American microblogging and social networking service.' },
+      { name: 'Company F', icon: 'logo-amazon', description: 'Amazon.com, Inc. is an American multinational technology company.' }
+    ]);
   }
   ngOnInit(): void {
     this.setCompanies()
