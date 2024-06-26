acc6e0f1535c9ea254761c9182a8f10344695ff1
demen <demenkdf@gmail.com>
2024-06-26 15:33:48 +0300

    add modal company desc

--- front-end/src/app/auth/authorizator/auth-form-wrapper/companies-marquee/companies-marquee.component.ts
+++ front-end/src/app/auth/authorizator/auth-form-wrapper/companies-marquee/companies-marquee.component.ts
@@ -1,7 +1,16 @@
 import {ChangeDetectionStrategy, Component, OnInit, signal, WritableSignal} from '@angular/core';
 import {NgForOf} from "@angular/common";
 import {IonicModule} from "@ionic/angular";
-import {IonButton, IonIcon, IonModal, IonPopover} from "@ionic/angular/standalone";
+import {
+  IonButton,
+  IonContent,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonModal,
+  IonPopover
+} from "@ionic/angular/standalone";
 
 @Component({
   selector: 'companies-marquee',
@@ -13,28 +22,35 @@ import {IonButton, IonIcon, IonModal, IonPopover} from "@ionic/angular/standalon
     IonIcon,
     IonModal,
     IonButton,
-    IonPopover
+    IonPopover,
+    IonList,
+    IonItem,
+    IonLabel,
+    IonContent
   ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CompaniesMarqueeComponent  implements OnInit {
 
   public isPaused: WritableSignal<boolean> = signal(false);
-  public companies: WritableSignal<{ name: string, icon: string, description: string }[]> = signal([]);
-  public selectedCompany: WritableSignal<{ name: string, icon: string, description: string } | null> = signal(null);
+  public companies: WritableSignal<{ name: string, icon: string, description: string, listings: string[], rating: number }[]> = signal([]);
+  public selectedCompany: WritableSignal<{ name: string, icon: string, description: string, listings: string[], rating: number } | null> = signal(null);
+
   public isPopoverOpen: WritableSignal<boolean> = signal(false);
   public popoverEvent: WritableSignal<Event | null> = signal(null)
+
 
   toggleAnimation(): void {
     this.isPaused.update((isPaused: boolean) => !isPaused);
   }
 
-  showCompanyDetails(event: Event, company: { name: string, icon: string, description: string }): void {
+  showCompanyDetails(event: Event, company: { name: string, icon: string, description: string, listings: string[], rating: number }): void {
     this.toggleAnimation()
     this.popoverEvent.set(event);
     this.selectedCompany.set(company);
     this.isPopoverOpen.set(true);
   }
+
 
   closePopover(): void {
     this.isPopoverOpen.set(false);
@@ -42,14 +58,27 @@ export class CompaniesMarqueeComponent  implements OnInit {
     this.toggleAnimation()
   }
 
+  getStars(rating: number | undefined): string[] {
+    if (!rating) return [];
+    const fullStars = Math.floor(rating);
+    const halfStars = rating % 1 !== 0 ? 1 : 0;
+    const emptyStars = 5 - fullStars - halfStars;
+
+    return [
+      ...Array(fullStars).fill('star'),
+      ...Array(halfStars).fill('star-half'),
+      ...Array(emptyStars).fill('star-outline')
+    ];
+  }
+
   setCompanies(): void {
     this.companies.set([
-      { name: 'Company A', icon: 'logo-apple', description: 'Apple Inc. is an American multinational technology company.' },
-      { name: 'Company B', icon: 'logo-google', description: 'Google LLC is an American multinational technology company.' },
-      { name: 'Company C', icon: 'logo-microsoft', description: 'Microsoft Corporation is an American multinational technology company.' },
-      { name: 'Company D', icon: 'logo-facebook', description: 'Facebook is an American online social media and social networking service.' },
-      { name: 'Company E', icon: 'logo-twitter', description: 'Twitter is an American microblogging and social networking service.' },
-      { name: 'Company F', icon: 'logo-amazon', description: 'Amazon.com, Inc. is an American multinational technology company.' }
+      { name: 'Company A', icon: 'logo-apple', description: 'Apple Inc. is an American multinational technology company.', listings: ['Car 1', 'Car 2'], rating: 4.5 },
+      { name: 'Company B', icon: 'logo-google', description: 'Google LLC is an American multinational technology company.', listings: ['Car 3', 'Car 4'], rating: 4 },
+      { name: 'Company C', icon: 'logo-microsoft', description: 'Microsoft Corporation is an American multinational technology company.', listings: ['Car 5', 'Car 6'], rating: 4.8 },
+      { name: 'Company D', icon: 'logo-facebook', description: 'Facebook is an American online social media and social networking service.', listings: ['Car 7', 'Car 8'], rating: 3.9 },
+      { name: 'Company E', icon: 'logo-twitter', description: 'Twitter is an American microblogging and social networking service.', listings: ['Car 9', 'Car 10'], rating: 4.3 },
+      { name: 'Company F', icon: 'logo-amazon', description: 'Amazon.com, Inc. is an American multinational technology company.', listings: ['Car 11', 'Car 12'], rating: 4.6 }
     ]);
   }
   ngOnInit(): void {
