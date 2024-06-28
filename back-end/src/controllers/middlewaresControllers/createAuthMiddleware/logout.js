b5e920f358c96d669c4dceb6e488c4102ce59474
stasbelinsky <stanislav.belinskyi@homeemergencyassist.com>
2024-06-28 20:50:31 +0300

    Logout - AWS settings

--- back-end/src/controllers/middlewaresControllers/createAuthMiddleware/logout.js
+++ back-end/src/controllers/middlewaresControllers/createAuthMiddleware/logout.js
@@ -5,7 +5,7 @@ const logout = async (req, res, { userModel }) => {
 
   const token = req.cookies.token;
   await UserPassword.findOneAndUpdate(
-    { user: req.admin._id },
+    { user: req.user._id },
     { $pull: { loggedSessions: token } },
     {
       new: true,
--- back-end/src/middlewares/uploadMiddleware/DoSingleStorage.js
+++ back-end/src/middlewares/uploadMiddleware/DoSingleStorage.js
@@ -6,20 +6,36 @@ const { slugify } = require('transliteration');
 const fileFilterMiddleware = require('./utils/fileFilterMiddleware');
 
 const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
+const null
 
-const secretAccessKey = process.env.DO_SPACES_SECRET;
-const accessKeyId = process.env.DO_SPACES_KEY;
-const endpoint = 'https://' + process.env.DO_SPACES_URL;
-const region = process.env.REGION;
+// const secretAccessKey = process.env.DO_SPACES_SECRET;
+// const accessKeyId = process.env.DO_SPACES_KEY;
+// const endpoint = 'https://' + process.env.DO_SPACES_URL;
+// const region = process.env.REGION;
+
+// const clientParams = {
+//   endpoint: endpoint,
+//   region: region,
+//   credentials: {
+//     accessKeyId,
+//     secretAccessKey,
+//   },
+// };
 
 const clientParams = {
-  endpoint: endpoint,
-  region: region,
+  endpoint: 'https://vsu-backet.s3.amazonaws.com/',
+  region: 'us-east-1',
   credentials: {
-    accessKeyId,
-    secretAccessKey,
+    accessKeyId: 'AKIA5FTZCQO7IXZGZXWA',
+    secretAccessKey: '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',
   },
+  requestHandler: new NodeHttpHandler({
+    httpsAgent: new https.Agent({
+      rejectUnauthorized: false,
+    }),
+  }),
 };
+
 
 const DoSingleStorage = ({
   entity,
