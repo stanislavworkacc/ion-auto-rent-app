53880f0185b2574e1a5163e6b7e42ed26aeb00cd
demen <demenkdf@gmail.com>
2024-07-04 17:20:25 +0300

    add vehicle imgs

--- front-end/src/app/shared/components/create-page/main-info/vehicle-type.service.ts
+++ front-end/src/app/shared/components/create-page/main-info/vehicle-type.service.ts
@@ -4,36 +4,42 @@ import {computed, effect, Injectable, Signal, signal, WritableSignal} from "@ang
   providedIn: 'root'
 })
 export class VehicleTypeService {
-  transportTypes: WritableSignal<{ label: string, value: string }[]> = signal([
+  transportTypes: WritableSignal<{ label: string, value: string, icon: string }[]> = signal([
     {
       label: 'Легкові',
       value: 'cars',
+      icon: '/assets/icon/car-type-ico.png'
     },
     {
       label: 'Вантажівки',
       value: 'trucks',
+      icon: '/assets/icon/truck-type-ico.png'
     },
     {
-      label: 'Мотоцикл',
+      label: 'Мотоцикли',
       value: 'motorcycles',
+      icon: '/assets/icon/moto-type-ico.png'
     },
     {
       label: 'Автобуси',
       value: 'buses',
+      icon: '/assets/icon/bus-type-ico.png'
     },
     {
       label: 'Спецтехніка',
       value: 'special',
+      icon: '/assets/icon/tractor-type-ico.png'
     },
     {
       label: 'Причепи',
       value: 'trailers',
+      icon: '/assets/icon/trailer-type-ico.png'
     },
   ]);
 
-  selectedValue: WritableSignal<{ label: string, value: string }> = signal({ label: '', value: '' })
+  selectedValue: WritableSignal<{ label: string, value: string, icon: string }> = signal({ label: '', value: '', icon: '' })
 
-  vehicleType: Signal<{ label: string, value: string }> = computed(() => this.selectedValue())
+  vehicleType: Signal<{ label: string, value: string, icon: string }> = computed(() => this.selectedValue())
 
   constructor() {
     effect((): void => {
