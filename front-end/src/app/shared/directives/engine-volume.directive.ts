5c67d1aed7488be2917101e5b562afa6fe7e2b58
demen <demenkdf@gmail.com>
2024-07-08 14:01:21 +0300

    handle engine blurring

--- front-end/src/app/shared/directives/engine-volume.directive.ts
+++ front-end/src/app/shared/directives/engine-volume.directive.ts
@@ -8,6 +8,21 @@ export class LimitEngineVolumeDirective {
 
   @Output() valueChange: EventEmitter<string> = new EventEmitter<string>();
   @HostListener('input', ['$event']) onInputChange(event) {
+    this.processInput(event);
+  }
+
+  @HostListener('ionBlur', ['$event']) onBlur(event) {
+    const input = this.el.nativeElement;
+    let value = input.value;
+
+    if (value.match(/^\d\.$/)) {
+      value += '0';
+      input.value = value;
+      this.valueChange.emit(value);
+    }
+  }
+
+  private processInput(event) {
     const input = this.el.nativeElement;
     let value = input.value;
 
@@ -17,12 +32,11 @@ export class LimitEngineVolumeDirective {
       value = value.substring(0, 1) + '.' + value.substring(1, 3);
     }
 
-    if (event.inputType === 'deleteContentBackward' && value === '.') {
+    if (event.inputType === 'deleteContentBackward') {
       value = '';
     }
 
     input.value = value;
-
     this.valueChange.emit(input.value);
 
     if (value !== input.value) {
