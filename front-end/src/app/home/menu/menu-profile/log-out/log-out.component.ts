00f4de756ebf5678e51eb0a8d4cd100c4655ea7b
demen <demenkdf@gmail.com>
2024-07-09 22:44:49 +0300

    add ripple tn

--- front-end/src/app/home/menu/menu-profile/log-out/log-out.component.ts
+++ front-end/src/app/home/menu/menu-profile/log-out/log-out.component.ts
@@ -1,15 +1,17 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {IonButton, IonIcon} from "@ionic/angular/standalone";
+import {RippleBtnComponent} from "../../../../shared/components/buttons/ripple-btn/ripple-btn.component";
 
 @Component({
   selector: 'log-out',
   templateUrl: './log-out.component.html',
   styleUrls: ['./log-out.component.scss'],
   standalone: true,
-  imports: [
-    IonButton,
-    IonIcon
-  ],
+    imports: [
+        IonButton,
+        IonIcon,
+        RippleBtnComponent
+    ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LogOutComponent  implements OnInit {
--- front-end/src/app/shared/components/buttons/ripple-btn/ripple-btn.component.ts
+++ front-end/src/app/shared/components/buttons/ripple-btn/ripple-btn.component.ts
@@ -1,17 +1,19 @@
 import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
-import {IonRippleEffect} from "@ionic/angular/standalone";
+import {IonIcon, IonRippleEffect} from "@ionic/angular/standalone";
 
 @Component({
   selector: 'ripple-btn',
   templateUrl: './ripple-btn.component.html',
   styleUrls: ['./ripple-btn.component.scss'],
   standalone: true,
-  imports: [IonRippleEffect],
+  imports: [IonRippleEffect, IonIcon],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RippleBtnComponent  implements OnInit {
 
   @Input({ required: true }) title: string = 'Підтвердити'
+  @Input() icon: string = ''
+  @Input() styles: string = ''
   constructor() { }
 
   ngOnInit() {}
