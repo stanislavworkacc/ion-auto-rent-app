d4ff840fdb6a282af31fe3786f92fc060717db75
demen <demenkdf@gmail.com>
2024-07-13 13:52:00 +0300

    clear breadcrubms

--- front-end/src/app/home/menu/car-park/in-rent/in-rent.component.ts
+++ front-end/src/app/home/menu/car-park/in-rent/in-rent.component.ts
@@ -1,16 +1,21 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {InRentAllSegmentComponent} from "../in-rent-all-segment/in-rent-all-segment.component";
-import {IonContent} from "@ionic/angular/standalone";
+import {IonButton, IonContent, IonIcon, IonLabel} from "@ionic/angular/standalone";
+import {BackButtonComponent} from "../../../../shared/ui-kit/components/back-button/back-button.component";
 
 @Component({
   selector: 'in-rent',
   templateUrl: './in-rent.component.html',
   styleUrls: ['./in-rent.component.scss'],
   standalone: true,
-  imports: [
-    InRentAllSegmentComponent,
-    IonContent
-  ],
+    imports: [
+        InRentAllSegmentComponent,
+        IonContent,
+        BackButtonComponent,
+        IonButton,
+        IonIcon,
+        IonLabel
+    ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class InRentComponent  implements OnInit {
