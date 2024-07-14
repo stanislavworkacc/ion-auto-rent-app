eb5e8233ec72bd156e6a3ece2c6ed637046787af
demen <demenkdf@gmail.com>
2024-07-14 21:10:56 +0300

    add deposti payment view

--- front-end/src/app/shared/components/create-page/rent-car-card/hour-rate-range/hour-rate-range.component.ts
+++ front-end/src/app/shared/components/create-page/rent-car-card/hour-rate-range/hour-rate-range.component.ts
@@ -1,10 +1,15 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import { IonInput, IonLabel} from "@ionic/angular/standalone";
 
 @Component({
   selector: 'hour-rate-range',
   templateUrl: './hour-rate-range.component.html',
   styleUrls: ['./hour-rate-range.component.scss'],
   standalone: true,
+  imports: [
+    IonLabel,
+    IonInput,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HourRateRangeComponent  implements OnInit {
