f137ef8237cf7ed9d1d504f5c9a5c7be1f28e8c8
demen <demenkdf@gmail.com>
2024-07-18 16:41:20 +0300

    add chat layout

--- front-end/src/app/home/chat/conversations-list/conversation-detail/conversation-detail.component.ts
+++ front-end/src/app/home/chat/conversations-list/conversation-detail/conversation-detail.component.ts
@@ -1,10 +1,20 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {IonContent, IonHeader, IonIcon, IonTitle, IonToolbar} from "@ionic/angular/standalone";
+import {RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-conversation-detail',
   templateUrl: './conversation-detail.component.html',
   styleUrls: ['./conversation-detail.component.scss'],
   standalone: true,
+  imports: [
+    IonContent,
+    IonHeader,
+    IonIcon,
+    IonTitle,
+    IonToolbar,
+    RouterOutlet
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ConversationDetailComponent  implements OnInit {
