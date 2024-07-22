3ae16119b4db4e932fac5d286975e9cf53c30c25
demen <demenkdf@gmail.com>
2024-07-22 19:32:54 +0300

    dashboard

--- front-end/src/app/shared/components/card-list/top-card-list/top-card-list.component.ts
+++ front-end/src/app/shared/components/card-list/top-card-list/top-card-list.component.ts
@@ -1,14 +1,23 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'top-card-list',
   templateUrl: './top-card-list.component.html',
   styleUrls: ['./top-card-list.component.scss'],
   standalone: true,
+  imports: [
+    NgForOf
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TopCardListComponent  implements OnInit {
 
+  topCards = [
+    { imageUrl: 'assets/icon/atlas-1.jpeg', title: 'Card 1' },
+    { imageUrl: 'assets/icon/atlas-2.jpg', title: 'Card 1' },
+    { imageUrl: 'assets/icon/atlas-3.jpg', title: 'Card 1' },
+  ];
   constructor() { }
 
   ngOnInit() {}
