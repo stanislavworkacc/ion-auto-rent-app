7025cfaf082feed24080888f779001176d10ac88
stasbelinsky <stanislav.belinskyi@homeemergencyassist.com>
2024-08-02 20:01:33 +0300

    Add delete account

--- back-end/src/routes/coreRoutes/coreAuth.js
+++ back-end/src/routes/coreRoutes/coreAuth.js
@@ -10,6 +10,7 @@ router.route('/login-google-sso').post(catchErrors(adminAuth.googleSsoLogin));
 router.route('/register').post(catchErrors(adminAuth.register));
 router.route('/matchPassword/:id').post(adminAuth.isValidAuthToken, catchErrors(adminAuth.matchPassword));
 router.route('/changePassword/:id').post(adminAuth.isValidAuthToken, catchErrors(adminAuth.changePassword));
+router.route('/deleteAccount/:id').post(adminAuth.isValidAuthToken, catchErrors(adminAuth.deleteAccount));
 
 router.route('/forgetpassword').post(catchErrors(adminAuth.forgetPassword));
 router.route('/resetpassword').post(catchErrors(adminAuth.resetPassword));
