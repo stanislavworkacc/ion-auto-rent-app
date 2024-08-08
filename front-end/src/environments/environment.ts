1f352e4f09efd97276384ec07b1e0a304c4de4fc
stasbelinsky <stanislav.belinskyi@homeemergencyassist.com>
2024-08-08 10:31:07 +0300

    Add env

--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -5,8 +5,8 @@
 
 // dev.rent-auto-app.site
 // export const domainName = 'http://143.198.120.163:8000';
-export const domainName = 'http://localhost:8000';
-// export const domainName = 'http://dev.rent-auto-app.site';
+// export const domainName = 'http://localhost:8000';
+export const domainName = 'https://rent-auto.xyz';
 
 export const environment = {
   GOOGLE_CLIENT_ID: '881739191835-niui5u8s5jb2r2b73ip38oon4bo53j93.apps.googleusercontent.com',
